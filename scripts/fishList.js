6690e2c24544da13d429959913b6760e786c25f1
Joey Blackwell <blackwellj6241@gmail.com>
2025-01-15 16:06:51 -0600

    Generated an HTML representation of each fish

--- scripts/fishList.js
+++ scripts/fishList.js
@@ -2,4 +2,24 @@ import { database } from './aquariumData.js';
 
 export const fishList = () => {
     // Generate an HTML representation of each fish
+    let fishes = database
+    let fishHTML = ''
+
+    for (const fish of fishes) {
+        fishHTML += `
+        <article class="fish">
+        <img src="${fish.image}" alt="${fish.name}">
+        <div class="fish_details">
+            <h2 class="fish_name">${fish.name}</h2>
+            <ol>
+            <li>${fish.species}</li>
+            <li>${fish.length}</li>
+            <li>${fish.location}</li>
+            <li>${fish.diet}</li>
+            </ol>
+        </div>
+        </article>
+        `
+    }
+    return fishHTML
 };
